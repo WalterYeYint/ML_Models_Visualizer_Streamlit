b290e78bd502b681278c15b0eb734126a5862fba
Kan Yu <WalterYeYint@gmail.com>
2021-12-22 10:40:27 +0630

    feat: create main.py, add drop-down boxes, sliders Created streamlit webapp and added boxes for datsets, classifiers. Also addd slider for each option.

---
+++ main.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from sklearn import datasets
+import numpy as np
+
+st.title("Streamlit Hey There")
+
+st.write("""
+# Explore different classifier
+Which one is the best?
+""")
+
+dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine Dataset"))
+st.write(dataset_name)
+
+classifier_name = st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Random Forest"))
+
+
+
+def get_dataset(dataset_name):
+	if dataset_name == "Iris":
+		data = datasets.load_iris()
+	elif dataset_name == "Breast Cancer":
+		data = datasets. load_breast_cancer()
+	else:
+		data = datasets.load_wine()
+	x = data.data
+	y = data.target
+	return x, y
+
+x, y = get_dataset(dataset_name)
+st.write("shape of dataset", x.shape)
+st.write("number of classes", len(np.unique(y)))
+
+def add_parameter_ui(clf_name):
+	params = dict()
+	if clf_name == "KNN":
+		K = st.sidebar.slider("K", 1, 15)
+		params["K"] = K
+	elif clf_name == "SVM":
+		C = st.sidebar.slider("C", 0.01, 10.0)
+		params["C"] = C
+	else:
+		max_depth = st.sidebar.slider("max depth", 2, 15)
+		n_estimators = st.sidebar.slider("number of estimators", 1, 100)
+		params["max_depth"] = max_depth
+		params["n_estimators"] = n_estimators
+	return params
+
+add_parameter_ui(classifier_name)
+
+
